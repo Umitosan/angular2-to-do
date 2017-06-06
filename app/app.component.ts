e60a4dc6f16d3390909f4808b5833ba4eeb794cf
Dominic Brown and Steven Galvin <steven@>
2017-06-06 14:12:44 -0700

    add one way data binding for priority color

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,18 +2,20 @@
 
 import { Component } from '@angular/core';
 
+// 'decorator'
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-    <h3>{{currentFocus}}</h3>
-    <ul>
-      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
-    </ul>
-  </div>
-  `
-})
+      <div class="container">
+       <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+       <h3>{{currentFocus}}</h3>
+       <ul>
+         <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)="editTask()">Edit!</button> </li>
+       </ul>
+     </div>
+    `
+  })
+
 
 // class declaration, how it behaves
 export class AppComponent {
@@ -23,13 +25,35 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
+    new Task('Finish weekend Angular homework for Epicodus course', 1),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 3)
   ];
+
+  editTask() {
+    alert("You just requested to edit a Task!");
+  }
+
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert("This task is done!");
+    } else {
+      alert("This task is not done. Better get to work!");
+    }
+  }
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 }
 
 export class Task {
   public done: boolean = false;
-  constructor(public description: string) { }
+  constructor(public description: string, public priority: number) { }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,3 +1,5 @@
+// This is the ROOT MODULE of our app
+
 // NgModule imports general Module code from the Angular framework's core.
 import { NgModule }       from '@angular/core';
 // BrowserModule imports code necessary to run our app in the browser, including built-in directives that allow us to add things like conditionals and loops to our components. We'll explore these soon.
@@ -5,11 +7,10 @@ import { BrowserModule }  from '@angular/platform-browser';
 // AppComponent actually refers to the root component we created! We call it AppComponent because that's the name of the class declaration exported at the bottom of its file.
 import { AppComponent }   from './app.component';
 
-
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent ], // declarations is an array of all components that will reside in this module.
+  bootstrap:    [ AppComponent ] // this is the root component and will be the first to show on the index page
 })
 
 export class AppModule { }
